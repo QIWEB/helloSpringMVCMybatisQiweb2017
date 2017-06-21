890b7ab63b4b7b1b7defce25fa1dcf6cd07b0d6a
QIWEB <zqijava@163.com>
2017-06-21 17:20:15 +0800

    spring-batch框架集成

---
+++ src/main/java/com/lyx/batch/ThrowExceptionProcessor.java
@@ -0,0 +1,22 @@
+package com.lyx.batch;
+
+import org.springframework.batch.item.ItemProcessor;
+public class ThrowExceptionProcessor{
+//public class ThrowExceptionProcessor implements
+//		ItemProcessor<People, PeopleDESC> {
+//
+//	public PeopleDESC process(People item) throws Exception {
+//		System.out.println("process people desc");
+//		if ("lyx".equals(item.getFirstName())) {
+//			throw new InvalidDataException("invalid data");
+//		}
+//		return new PeopleDESC(item.getLastName(), item.getFirstName(), Thread
+//				.currentThread().getName());
+//	}
+//
+//	@Override
+//	public PeopleDESC process(People arg0) throws Exception {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+}
